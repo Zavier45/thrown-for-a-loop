f62e8f884284f4dbe52c648aea055745e6334dd4
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-02 15:16:08 -0500

    updated logic to include Product class

--- Program.cs
+++ Program.cs
@@ -2,19 +2,51 @@
 string greeting = @"Welcome to Thrown for a Loop
 Your one-stop shop for used sporting equipment";
 Console.WriteLine(greeting);
-List<string> products = new List<string>()
+List<Product> products = new List<Product>()
 {
-    "Football",
-    "Hockey Stick",
-    "Boomerang",
-    "Frisbee",
-    "Golf Putter"
+    new Product()
+    {
+        Name ="Football",
+        Price = 15,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Hockey Stick",
+        Price = 12,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Soccor Ball",
+        Price = 15,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Frisbee",
+        Price = 5,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Golf Club",
+        Price = 18,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Volleyball",
+        Price = 13,
+        Sold = false
+    }
+    //curly braces serve as an 'object initializer'
 };
 
 Console.WriteLine(@"Products:");
 for (int i = 0; i < products.Count; i++)
 {
-    Console.WriteLine($"{i + 1}. {products[1]}");
+    Console.WriteLine($"{i + 1}. {products[i].Name}");
 }
 Console.WriteLine("Please enter a product number: ");
 int response = int.Parse(Console.ReadLine().Trim());
@@ -25,5 +57,6 @@ while (response > products.Count || response < 1)
 }
 
 
-Console.WriteLine(@"You chose: {products[response - 1]}");
+Product chosenProduct = products[response - 1];
+Console.WriteLine($"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars and is {(chosenProduct.Sold ? "" : "not")} sold.");
 
