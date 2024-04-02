3839ede24cc737a24a89ab7dcfb97d335a0dfaaf
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-02 11:01:53 -0500

    changed if to while

--- Program.cs
+++ Program.cs
@@ -4,5 +4,13 @@ Your one-stop shop for used sporting equipment";
 Console.WriteLine(greeting);
 Console.WriteLine("Please enter a product name: ");
 string response = Console.ReadLine();
+while (string.IsNullOrEmpty(response))
+{
+    Console.WriteLine("You didn't choose anything, try again!");
+    response = Console.ReadLine();
+}
+
+
 Console.WriteLine(@$"You chose: {response}.
 Thank you for your input!");
+
